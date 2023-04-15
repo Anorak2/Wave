5ceb1331c167dae19d1d2dda45bcdec9e3cbb283
Ellie <mostdigitsofpi@gmail.com>
2023-04-15 16:33:13 -0500

    add wallet config page

--- main-ui/devmode-button.js
+++ main-ui/devmode-button.js
@@ -8,39 +8,38 @@ function checkDevMode(){
         browser.storage.local.set({isDevMode: false});
     }
     function gotDevModeStatus(results){
-/*        if (typeof(results["isDevMode"]) === 'undefined'){
-            browser.storage.local.set({isDevMode: false});
-            console.log("No devmode status; defaulted to false.");
-            results["isDevMode"] = false;
-        }*/
-        console.log(results["isDevMode"]);
+        //console.log(results["isDevMode"]);
         if (results["isDevMode"]){
             document.getElementById("devModeToggle").innerHTML = "enabled";
+            Array.from(document.getElementsByClassName("devmode-show")).forEach( s => {
+                s.style.display="revert";
+            });
+            Array.from(document.getElementsByClassName("devmode-hide")).forEach( s => {
+                s.style.display="none";
+            });
         } else{
             document.getElementById("devModeToggle").innerHTML = "disabled";
-        }
-    }
-    let inDevMode = browser.storage.local.get("isDevMode");
-    inDevMode.then((results) => gotDevModeStatus(results));
-}function switchDevMode(){
-    function gotDevModeStatus(results){
-/*        if (typeof(results["isDevMode"]) === 'undefined'){
-            browser.storage.local.set({isDevMode: false});
-            console.log("No devmode status; defaulted to false.");
-            results["isDevMode"] = false;
-        }*/
-        console.log(results["isDevMode"]);
-        if (results["isDevMode"]){
-            browser.storage.local.set({isDevMode: false});
-            document.getElementById("devModeToggle").innerHTML = "disabled";
-        } else{
-            browser.storage.local.set({isDevMode: true});
-            document.getElementById("devModeToggle").innerHTML = "enabled";
+            Array.from(document.getElementsByClassName("devmode-show")).forEach( s => {
+                s.style.display="none";
+            });
+            Array.from(document.getElementsByClassName("devmode-hide")).forEach( s => {
+                s.style.display="revert";
+            });
         }
     }
     let inDevMode = browser.storage.local.get("isDevMode");
     inDevMode.then((results) => gotDevModeStatus(results));
 }
 
-
+function switchDevMode(){
+    let inDevMode = browser.storage.local.get("isDevMode");
+    inDevMode.then((results) => {
+        if (results["isDevMode"]){
+            browser.storage.local.set({isDevMode: false});
+        } else{
+            browser.storage.local.set({isDevMode: true});
+        }
+        checkDevMode();
+    });
+}
 checkDevMode();
