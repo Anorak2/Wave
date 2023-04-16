ef75e9fa399cd9e7495688a15c7b00bb8efe8a5f
Anorak <55265984+Anorak2@users.noreply.github.com>
2023-04-15 23:36:20 -0500

    grabbing amount to donate from user

--- main-ui/homepage-scripts.js
+++ main-ui/homepage-scripts.js
@@ -58,7 +58,7 @@ function checkDonations(){
 }
 
 function donateNow(){
-	alert("Eventually we'll have some code and this'll confirm the donation");
+  alert("we dont have this right now, come back later")
 }
     
 function checkWebsites(){
@@ -82,27 +82,40 @@ function checkPayments(){
     document.getElementById("backButton").style.display = "block";
     document.getElementById("pageTitle").innerHTML = "Donation settings";
     
-    document.getElementById("3a").style.display = "block";
-    document.getElementById("3b").style.display = "block";
-    //Get wallet
-    document.getElementById("3c").style.display = "block";
     //That's a whole thing
     document.getElementById("3d").style.display = "block";
     document.getElementById("3e").style.display = "block";
     //Get user history
     document.getElementById("3f").style.display = "block";
-    document.getElementById("3g").style.display = "block";
     //Get user history
     document.getElementById("3h").style.display = "block";
-    document.getElementById("3i").style.display = "block";
 }
 
 function addRipple(){
 	alert("Eventually we'll have some code and this'll confirm the addition of XRPL");
 }
 
-function updateDon(){
-	alert("Eventually we'll have some code and this'll confirm the donation amount");
+function updateDonAmt(){
+  var x = document.getElementById("3f").value;
+  x = parseFloat(x)
+  let donationAmt = {
+    amtXRP: x
+  }
+  browser.storage.local.set(donationAmt)
+  alert("Your new donation amount is " + x + " XRP")
+  updateXrpCount()
+}
+
+
+function XrpCountOnGot(item){
+  z = "" + item.donationAmt.amtXRP.toString() + " XRP";
+  document.getElementById("3e").innerHTML = z;
+
+}
+async function updateXrpCount(){
+  let storageItem = browser.storage.local.get("donationAmt");
+  storageItem.then((result) => XrpCountOnGot(result), onError);
+  console.log(browser.storage.local.get())
 }
 
 function goBack(){
@@ -123,21 +136,18 @@ function goBack(){
 	  document.getElementById("2a").style.display = "none";
     document.getElementById("weekTable").style.display = "none";
     
-    document.getElementById("3a").style.display = "none";
-    document.getElementById("3b").style.display = "none";
-    document.getElementById("3c").style.display = "none";
     document.getElementById("3d").style.display = "none";
     document.getElementById("3e").style.display = "none";
     document.getElementById("3f").style.display = "none";
-    document.getElementById("3g").style.display = "none";
     document.getElementById("3h").style.display = "none";
-    document.getElementById("3i").style.display = "none";
 }
 
-document.getElementById("3g").innerHTML = document.getElementById("3f").value;
-document.getElementById("3f").oninput = function() {
-  document.getElementById("3g").innerHTML = "New value: " + document.getElementById("3g").value;
+// This inputs the new amount to donate each week
+/*document.getElementById("3h").oninput = function() {
+  document.getElementById("3g").innerHTML = document.getElementById("3f").value;
+  alert("dumbo")
 }
+*/
 //Connect to a list of websites
 //Connect to the wallet
 
@@ -162,10 +172,6 @@ document.addEventListener('DOMContentLoaded', function () {
           .addEventListener('click', function (){donateNow()});
 });
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('3c')
-          .addEventListener('click', function (){addRipple()});
-});
-document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('3h')
-          .addEventListener('click', function (){updateDon()});
+          .addEventListener('click', function (){updateDonAmt()});
 });
